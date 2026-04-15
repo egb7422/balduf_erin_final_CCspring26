83c1936ed4fa52df11bb5561b876d87843124163
egb7422 <egb7422@nyu.edu>
2026-04-15 12:44:32 -0400

    starter file

---
+++ sketch.js
@@ -0,0 +1,18 @@
+function setup() {
+ // put setup code here
+createCanvas(1600,700);
+background(255,0,255);
+
+}
+
+function draw() {
+  // put drawing code here
+
+}
+
+function mousePressed () {
+background(0,200,200);
+print("hello");
+
+
+}
