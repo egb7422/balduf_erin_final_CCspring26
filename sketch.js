d5933a1a4dbc45f5fff51bbd351bc8d65445ffd9
egb7422 <egb7422@nyu.edu>
2026-04-26 09:32:51 -0400

    setup

--- sketch.js
+++ sketch.js
@@ -1,12 +1,19 @@
-function setup() {
-createCanvas(windowWidth,windowHeight);
-background(0);
+// basically in the end add sound library so that keyboard plays
+// prototype of keyboard visuals
+//   textStyle(BOLD); // p5 example from https://p5js.org/reference/p5/textStyle/
+
+let key = ''; //var stores the current key being pressed
+
+function setup(){
+
+  createCanvas(windowWidth, windowHeight); // makes canvas the size of the window
+
+  textAlign(CENTER,CENTER); // p5 example: https://p5js.org/reference/p5/textAlign/ -> centers text horizontally and vertically
+  textSize(32); // size of text, p5 example: https://p5js.org/reference/p5/textSize/
+
+
 
 }
 
-function draw() {
-  // put drawing code here
-
-}
 
 
